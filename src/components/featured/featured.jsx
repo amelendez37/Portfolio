fec1b5f8e64a22a47fa39e23f64de7430d320ea6
Aaron Melendez <aaronmelendez37@gmail.com>
2018-10-11 15:48:38 -0700

    Add Project component

---
+++ src/components/featured/featured.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Styled from "styled-components";
+
+import Project from "./project/project.jsx";
+import TaskTrackerVid from "../../../public/images/tasktracker_example.mov";
+
+const Container = Styled.div`
+  margin: 7rem 0;
+`;
+
+const Featured = () => {
+  return (
+    <Container>
+      <Project
+        title={"Task Tracker"}
+        summary={"A tool that improves workflows."}
+        details={"React, React-dnd, Node, Express, MongoDB"}
+        link={"https://bit.ly/2IObIKi"}
+        source={TaskTrackerVid}
+      />
+    </Container>
+  );
+};
+
+export default Featured;
