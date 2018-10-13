b96a45945dc63f55da0d6f38152f028e9229fa0b
Aaron Melendez <aaronmelendez37@gmail.com>
2018-10-12 18:20:59 -0700

    Separate content into two separate cards

--- src/components/about/about_content/aboutContent.jsx
+++ src/components/about/about_content/aboutContent.jsx
@@ -3,35 +3,57 @@ import Styled from "styled-components";
 
 import ProfileImg from "../../../../public/images/profile.png";
 
-const textColor = `#626567`;
+const textColor = "#626567";
 
-const Container = Styled.div`
-  width: 75%;
-  border: 2px solid #EAF2F8;
-  margin: 7rem 0 10rem 50%;
-  transform: translateX(-50%);
+const Cards = Styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  margin: 10% 0;
+`;
+
+const Card = Styled.div`
+  width: 37%;
+  box-shadow: 0 4px 12px 0 rgba(0,0,0,.25);
   text-align: center;
 `;
 
 const Profile = Styled.img`
-  padding: 5rem 0;
+  margin-top: 4rem;
   border-radius: 50%;
   width: 8.5rem;
 `;
 
 const Text = Styled.p`
-  width: 60%;
+  width: 70%;
   margin: 0 0 0 50%;
-  padding-bottom: 7rem;
+  padding: 6rem 0 3rem 0;
   transform: translateX(-50%);
   font-size: 1.5rem;
   color: ${textColor};
   line-height: 3rem;
 `;
 
-const Footer = Styled.div`
-  background-color: #F2F2F2;
+const TextRightTitle = Styled.p`
+  font-size: 3rem;
   color: ${textColor};
+  padding: 3rem 0 5rem 0;
+`;
+
+const TextContainerLeft = Styled.div`
+  width: 100%;
+  background-color: #F2F3F4;
+  clip-path: polygon(0 100%, 100% 100%, 100% 0, 0 18%);
+`;
+
+const TextContainerRight = Styled.div`
+  width: 100%;
+  background-color: #F2F3F4;
+  clip-path: polygon(0 100%, 100% 55%, 100% 0, 0 0);
+`;
+
+const Footer = Styled.div`
+  background-color: #839192;
+  color: #F0F3F4;
   width: 100vw;
   height: 4rem;
   text-align: center;
@@ -47,20 +69,26 @@ const FooterText = Styled.p`
 const AboutContent = () => {
   return (
     <React.Fragment>
-      <Container>
-        <Profile src={ProfileImg} alt="Picture of me" />
-        <Text>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industrys standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum.
-        </Text>
-      </Container>
+      <Cards>
+        <Card>
+          <Profile src={ProfileImg} alt="Picture of me" />
+          <TextContainerLeft>
+            <Text>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industrys standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has
+              survived not only.
+            </Text>
+          </TextContainerLeft>
+        </Card>
+        <Card>
+          <TextContainerRight>
+            <TextRightTitle>SKILLS</TextRightTitle>
+          </TextContainerRight>
+          <Text>LIST SKILLS</Text>
+        </Card>
+      </Cards>
       <Footer>
         <FooterText>
           Photos by Raul Varzr, Pankaj Patel, and Benjamin Sow on Unsplash
