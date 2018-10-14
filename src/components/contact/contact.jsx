dbac9ec0b450fa3556b620fbfbf94d59fb8ce5ec
Aaron Melendez <aaronmelendez37@gmail.com>
2018-10-13 22:41:16 -0700

    Complete Contact component

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import Styled from "styled-components";
 
 import PhoneImg from "../../../public/images/phone.jpg";
+import Email from "../../../public/images/envelop.svg";
+import Github from "../../../public/images/github.svg";
+import Linkedin from "../../../public/images/Linkedin.svg";
 
 const Container = Styled.div`
   position: relative;
@@ -9,10 +12,12 @@ const Container = Styled.div`
 `;
 
 const Image = Styled.img`
+  display: block;
   width: 100%;
+  filter: brightness(90%);
 `;
 
-const Text = Styled.p`
+const Title = Styled.p`
   position: absolute;
   top: 10rem;
   left: 10rem;
@@ -21,26 +26,54 @@ const Text = Styled.p`
   color: #fff;
 `;
 
-const Contacts = Styled.ul`
+const Contacts = Styled.div`
   position: absolute;
   top: 23rem;
-  left: 13rem;
+  left: 12rem;
   list-style: none;
 `;
 
-const ContactItem = Styled.li`
+const ContactItem = Styled.a`
   color: #fff;
+  font-size: 1.5rem;
+  margin-left: 2rem;
+`;
+
+const SubContainer = Styled.div`
+  display: flex;
+  align-items: center;
+  margin: 4rem 0;
+`;
+
+const Icon = Styled.img`
+  filter: invert(1);
 `;
 
 const Contact = () => {
   return (
     <Container>
       <Image src={PhoneImg} />
-      <Text>Get in touch.</Text>
+      <Title>Get in touch.</Title>
       <Contacts>
-        <ContactItem>Email</ContactItem>
-        <ContactItem>LinkedIn</ContactItem>
-        <ContactItem>Github</ContactItem>
+        <SubContainer>
+          <Icon src={Email} alt={"Email icon"} />
+          <ContactItem>aaronmelendez37@gmail.com</ContactItem>
+        </SubContainer>
+        <SubContainer>
+          <Icon src={Linkedin} alt={"LinkedIn icon"} />
+          <ContactItem
+            href="https://www.linkedin.com/in/aaron-melendez/"
+            target="_blank"
+          >
+            https://www.linkedin.com/in/aaron-melendez/
+          </ContactItem>
+        </SubContainer>
+        <SubContainer>
+          <Icon src={Github} alt={"Github icon"} />
+          <ContactItem href="https://github.com/amelendez37" target="_blank">
+            https://github.com/amelendez37
+          </ContactItem>
+        </SubContainer>
       </Contacts>
     </Container>
   );
