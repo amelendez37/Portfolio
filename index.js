88393a503a43f7c5bd99be70da34916a6ab5ded8
Aaron Melendez <aaronmelendez37@gmail.com>
2018-10-15 15:12:48 -0700

    Remove use of Morgan module

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const express = require("express");
-const morgan = require("morgan");
 
 const app = express();
 const port = 3000;
 
-app.use(morgan("tiny"));
 app.use(express.static("./dist"));
 
 app.listen(port);
