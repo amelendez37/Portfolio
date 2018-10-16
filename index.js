ab3c577734e9d0c92e6571cbd516685c1e28e837
Aaron Melendez <aaronmelendez37@gmail.com>
2018-10-16 13:20:47 -0700

    Edit path to static files

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static("./dist"));
+app.use(express.static(path.join(__dirname, "./dist")));
 
 app.listen(port);
