20efd43b4fed08b238b907d38e570b2413eb3d23
Aaron Melendez <aaronmelendez37@gmail.com>
2018-10-18 18:04:48 -0700

    Add wild card route

--- index.js
+++ index.js
@@ -5,5 +5,6 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, "./dist")));
+app.use("*", (req, res) => res.redirect("/"));
 
 app.listen(port);
